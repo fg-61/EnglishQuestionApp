e328c4abe85ae5156c5eda904bbc7c3c74239d21
Fatih Gören <fatihgoren61@gmail.com>
2022-03-26 21:06:56 +0300

    Add Test View Page

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using HtmlAgilityPack;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace EnglishQuestionApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //HtmlDocument mostRecentDocument;
+            //string urlHome = "https://www.wired.com/";
+            //HtmlWeb web = new HtmlWeb();
+            //HtmlDocument doc = web.Load(urlHome);
+
+            return View();
+        }
+    }
+}
