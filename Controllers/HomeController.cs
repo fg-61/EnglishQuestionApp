363f2ac50833a4155d244e774c047d1ba2e98989
Fatih Gören <fatihgoren61@gmail.com>
2022-03-28 19:22:06 +0300

    Get Data Complete

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using HtmlAgilityPack;
+﻿using EnglishQuestionApp.ViewModels;
+using HtmlAgilityPack;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Net;
+using System.Text;
 
 namespace EnglishQuestionApp.Controllers
 {
@@ -8,12 +11,47 @@ namespace EnglishQuestionApp.Controllers
     {
         public IActionResult Index()
         {
-            //HtmlDocument mostRecentDocument;
-            //string urlHome = "https://www.wired.com/";
-            //HtmlWeb web = new HtmlWeb();
-            //HtmlDocument doc = web.Load(urlHome);
+            string urlHome = "https://www.wired.com";
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(urlHome);
 
-            return View();
+            List<MostRecentViewModel> mostRecents = new List<MostRecentViewModel>();
+
+            for (int i=1; i<=2; i++)
+            {
+                List<string> paragraphs = new List<string>();
+
+                HtmlNode titleNode = doc.DocumentNode.SelectSingleNode("//*[@id='main-content']/div[1]/div[1]/section/div[3]/div/div/div/div/div[" + i + "]/div[2]/a/h2");
+                if (titleNode != null)
+                {
+                    string title = titleNode.InnerText;
+                    // Get Content
+                    HtmlNode contentLinkNode = doc.DocumentNode.SelectSingleNode("//*[@id='main-content']/div[1]/div[1]/section/div[3]/div/div/div/div/div[" + i + "]/div[2]/a[@href]");
+                    string contentHrefValue = contentLinkNode.GetAttributeValue("href", string.Empty);
+                    string contentUrl = urlHome + contentHrefValue;
+                    HtmlDocument contentDoc = web.Load(contentUrl);
+                    HtmlNodeCollection contentNodes = contentDoc.DocumentNode.SelectNodes("//*[@id='main-content']/article/div[2]/div[1]/div[1]/div[1]/div[1]/div/div/p");
+                    foreach (HtmlNode contentNode in contentNodes)
+                    {
+                        string content = contentNode.InnerText;
+                        string decodedstring = WebUtility.HtmlDecode(content); // Fix the special characters
+                        paragraphs.Add(decodedstring);
+                    }
+                    mostRecents.Add(new MostRecentViewModel
+                    {
+                        Title = title,
+                        Paragraphs = paragraphs
+                    });
+                }
+            }
+            return View(mostRecents);
+        }
+
+        [HttpPost]
+        public IActionResult GetText(List<MostRecentViewModel> texts, int textNumber)
+        {
+            List<string> paragraphs = texts[textNumber].Paragraphs;
+            return View(paragraphs);
         }
     }
 }
---
+++ ViewModels/MostRecentViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace EnglishQuestionApp.ViewModels
+{
+    public class MostRecentViewModel
+    {
+        public string Title { get; set; }
+        public List<string> Paragraphs { get; set; }
+    }
+}
